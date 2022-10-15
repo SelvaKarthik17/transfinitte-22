d78dd2da3cd1863fd04896fd0dca50f192313b32
Pradeep S <63255407+pradeep-707@users.noreply.github.com>
2022-10-16 00:10:05 +0530

    feat: connected backend with engine (#4)
    Co-authored-by: ashwath niranjh <ashwathniranjh@gmail.com>

---
+++ backend/engine/engine.py
@@ -0,0 +1,31 @@
+from t2d2 import convert_to_tree_format, get_structured_house_data
+from indgov_search import get_ind_gov_details
+from utils import get_electoral_roll_pdf_url, get_pdf_from_url, create_temp_folder
+from text_recognition import get_text_from_pdf
+from translate import translate_locale_out
+import os
+
+def engine_of_program(name, age, state, voter_id, gender, relation_name):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'key.json'
+
+    create_temp_folder()
+    user_data = {
+                'name': name,
+                'rln_name': relation_name,
+                'location': 'S22,,',
+                'age': age,
+                'gender': gender,
+            }
+    data = get_ind_gov_details(user_data)
+    PDF_PATH = 'temp/test.pdf'
+
+    pdf_url = get_electoral_roll_pdf_url(data)
+    pdf = get_pdf_from_url(pdf_url)
+    with open(PDF_PATH, 'wb') as f:
+        f.write(pdf)
+    get_text_from_pdf(PDF_PATH)
+    translate_locale_out()
+    house_data = get_structured_house_data(data['slno_inpart'])
+    tree_data = convert_to_tree_format(house_data)  
+
+    return tree_data
