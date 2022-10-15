5088dc7e775080d01fda51454b916d063304a41b
Arnav Menon <arnavmenonr@gmail.com>
2022-10-16 02:42:30 +0530

    Feat: Added form submit process, dynamic family tree data

--- frontend/src/pages/GetFamily.js
+++ frontend/src/pages/GetFamily.js
@@ -1,37 +1,78 @@
 import React, { Component } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import { LockClosedIcon } from '@heroicons/react/solid'
+//import { LockClosedIcon } from '@heroicons/react/solid'
+import axios from 'axios';
 export default function GetFamily() {
 
     const [states, setStates] = React.useState(["Tamil Nadu"]);
+    const [name, setName] = React.useState("");
+    const [fatherName, setFatherName] = React.useState("");
+    const [state, setState] = React.useState("");
+    const [age, setAge] = React.useState("");
+    const [vid, setVid] = React.useState("");
+    const [gender, setGender] = React.useState("");
+
+    const handleSubmit = (e) => {
+        
+        e.preventDefault();
+        // replace states with state codes if needed
+        //tn is S22
+        let reqBody = {
+            name: name,
+            relation_name: fatherName,
+            state: state,
+            age: age,
+            voter_id: vid,
+            gender: gender,
+        }
+
+        console.log(reqBody);
+        axios
+        .post("https://transfinitte-api.selvakarthik.me/", reqBody)
+        .then((response)=>{
+            console.log(response);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+
+        alert("Finding election records for you...");
+    }
 
     return (
             <div>
-                <form className='flex flex-col items-center justify-center m-2'>
+                <form className='flex flex-col items-center justify-center m-2' onSubmit = {handleSubmit}>
                 <div className='w-3/5 m-2 flex justify-around items-center'>
                 <label for="name">Enter your name:</label>
-                    <input required name='name' type="text" placeholder="Enter name" className='p-2 border  border-3 rounded-md' />
+                    <input required name='name' type="text" placeholder="Enter name" className='p-2 border border-3 rounded-md' onChange = {(e) => setName(e.target.value)}/>
                     </div>
                     <div className='w-3/5 m-2 flex justify-around items-center'>
                 <label for="fatherName">Enter your Father's name:</label>
-                    <input required name = 'fatherName' type="text" placeholder="Enter name" className='p-2 border  border-3 rounded-md' />
+                    <input required name = 'fatherName' type="text" placeholder="Enter father's name" className='p-2 border  border-3 rounded-md' onChange = {(e) => setFatherName(e.target.value)} />
                     </div><div className='w-3/5 m-2 flex justify-around items-center'>
                 <label for="">Enter your state:</label>
-                    <input required name = 'state' type="text" placeholder="Enter name" className='p-2 border  border-3 rounded-md' />
-                    </div><div className='w-3/5 m-2 flex justify-around items-center'>
+                <select className='p-2 border border-1 w-1/5' onChange = {(e) => setState(e.target.value)}>
+                    <option disabled selected value>Select state</option>
+                    {states.map((state) => {
+                        return <option value={state}>{state}</option>
+                    })}
+                </select>
+
+                    </div> <div className='w-3/5 m-2 flex justify-around items-center'>
                 <label for="age">Enter your age:</label>
-                    <input required type="text" placeholder="Enter name" className='p-2 border  border-3 rounded-md' />
+                    <input required type="number" min={18}  className='p-2 border  border-3 rounded-md' onChange = {(e) => setAge(e.target.value)}/>
                     </div>
                     <div className='w-3/5 m-2 flex justify-around items-center'>
                 <label for="">Enter your voter ID:</label>
-                    <input name = 'voterId' type="text" placeholder="Enter name" className='p-2 border  border-3 rounded-md' />
+                    <input name = 'voterId' type="text" placeholder="Enter voter ID" className='p-2 border  border-3 rounded-md' onSelect = {(e) => setVid(e.target.value)}/>
                     </div>
                     <div className='w-3/5 m-2 flex justify-around items-center'>
                 <label for="gender">Enter your gender:</label>
-                <select className='p-2 border border-1 w-1/5'>
-                    <option value="male">male</option>
-                    <option value="female">female</option>
+                <select className='p-2 border border-1 w-1/5' onChange = {(e) => setGender(e.target.value)}>
+                    <option disabled selected value>Select gender</option>
+                    <option value="M">male</option>
+                    <option value="F">female</option>
                 </select>
                     </div>
                     
--- frontend/src/pages/Tree.js
+++ frontend/src/pages/Tree.js
@@ -1,16 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import FamilyTree from "../components/Mytree";
 
-export default class Tree extends Component {
-  render() {
-    return (
+export default function Tree(props){
+
+    const [familyData, setFamilyData] = React.useState([]);
+    const temp = [{"id": 0, "name": "Nagaratnam", "pids": [], "gender": "female"}, {"id": 1, "name": "Sreedharan", "pids": [2], "gender": "male"}, {"id": 2, "name": "Maheshwari", "pids": [1], "gender": "female"}, {"id": 3, "name": "Sulaiman", "pids": [], "gender": "male", "fid": 4}, {"id": 4, "name": "Syed Alivi", "pids": [], "gender": "male"}, {"id": 5, "name": "Srinivasaragavan", "pids": [6], "gender": "male", "fid": 1, "mid": 2}, {"id": 6, "name": "Revathi", "pids": [5], "gender": "female"}, {"id": 7, "name": "Nandini", "pids": [], "gender": "female", "fid": 9}, {"id": 8, "name": "Arunkarthik", "pids": [], "gender": "male"}, {"id": 9, "name": "Ganesan", "pids": [], "gender": "male"}, {"id": 10, "name": "Saranyaphanu", "pids": [], "gender": "female"}, {"id": 11, "name": "Saravanan", "pids": [], "gender": "male", "fid": 12, "mid": 13}, {"id": 12, "name": "Pattappasamy", "pids": [13], "gender": "male"}, {"id": 13, "name": "Sumathi", "pids": [12], "gender": "female"}, {"id": 14, "name": "Bhupathiraj", "pids": [], "gender": "male", "fid": 12, "mid": 13}, {"id": 15, "name": "Kalachelvi", "pids": [], "gender": "female"}, {"id": 16, "name": "Gomati", "pids": [19], "gender": "female"}, {"id": 17, "name": "Soundaryabala", "pids": [], "gender": "female", "fid": 19, "mid": 16}, {"id": 18, "name": "Balasubramaniam", "pids": [], "gender": "male", "fid": 19, "mid": 16}, {"id": 19, "name": "Ramamurthy", "pids": [16], "gender": "male"}, {"id": 20, "name": "Kausalya", "pids": [], "gender": "female"}, {"id": 21, "name": "Sandhya", "pids": [], "gender": "female"}, {"id": 22, "name": "Ramya", "pids": [25], "gender": "female"}, {"id": 23, "name": "Arumugam", "pids": [], "gender": "male"}, {"id": 24, "name": "Muthukumar", "pids": [], "gender": "male"}, {"id": 25, "name": "Chandramouleswaran", "pids": [22], "gender": "male"}, {"id": 26, "name": "Satyakokila", "pids": [], "gender": "female"}, {"id": 27, "name": "Gandhi", "pids": [], "gender": "female"}, {"id": 28, "name": "Shanti", "pids": [], "gender": "female"}, {"id": 29, "name": "Saipreeta", "pids": [], "gender": "female"}]
+    useEffect(() => {
+        setFamilyData(props.familyData);
+        console.log(familyData);
+    }, [props.familyData]);
+
+    return (  
       <div className=' w-3/4 border border-1 my-4 mx-auto'>
         <FamilyTree
-          nodes={[{'id': 0, 'name': ' Raghunath Singh ', 'pids': [1], 'gender': 'male'}, {'id': 1, 'name': ' Nannubai ', 'pids': [0], 'gender': 'female'}, {'id': 2, 'name': ' Yuvraj ', 'pids': [], 'gender': 'male', 'fid': 0}]}
+          nodes={temp}
         />
       </div>
     );
-  }
 }
