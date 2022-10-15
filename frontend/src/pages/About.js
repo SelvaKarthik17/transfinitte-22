3c3feab0d7ac4f84bf008fe1cec17f5181023319
ashwath niranjh <ashwathniranjh@gmail.com>
2022-10-15 13:31:36 +0530

    Feat: Design changes of footer and about page, moved tree to home page.

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -6,54 +6,96 @@ const About = () => {
   return (
     <div className='h-98 w-98'>
       <Header />
-      <div className='flex justify-center items-center h-1/5 intro'>
+      <div className='flex justify-center items-center h-1/5 my-8 intro'>
         We are a team of enthusiastic individuals united for a common cause.
         <br />
         The pursuit of our undying love for coding.
       </div>
       <div className='flex flex-col justify-center items-center w-full members'>
-        Presenting to you, our team members
-        <div className='w-full flex flex-col items-center'>
-          <div className=' my-4 p-4 w-3/4 bg-gray-100 flex items-center justify-between'>
+        Presenting to you, <b>Our Team Members</b>
+        <div className='w-4/5 grid grid-cols-2 items-center mt-4 mb-16'>
+          <div className=' m-4 p-4 bg-gray-100 flex items-center justify-between'>
             <img
               src='https://i.ibb.co/whGTTsq/1586501225793.jpg'
               className='h-64 mr-8'
+              alt='profile'
             />
             <div className='desc'>
+              <div className='name font-semibold'>Ashwath Niranjh</div>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
               viverra metus eget turpis rutrum, sit amet ullamcorper leo
               efficitur. Fusce efficitur sapien ac erat dignissim, quis rutrum
-              ligula porta. In semper nisi quam, in egestas leo ultrices quis.
-              In consectetur ultrices aliquet. Nam posuere pulvinar lorem, in
-              fringilla sapien volutpat at.
+              ligula porta.
             </div>
           </div>
-          <div className=' my-4 p-4 w-3/4 bg-gray-100 flex items-center justify-between'>
+          <div className=' m-4 p-4 bg-gray-100 flex items-center justify-between'>
             <img
               src='https://i.ibb.co/whGTTsq/1586501225793.jpg'
               className='h-64 mr-8'
+              alt='profile'
             />
             <div className='desc'>
+              <div className='name font-semibold'>Ashwath Niranjh</div>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
               viverra metus eget turpis rutrum, sit amet ullamcorper leo
               efficitur. Fusce efficitur sapien ac erat dignissim, quis rutrum
-              ligula porta. In semper nisi quam, in egestas leo ultrices quis.
-              In consectetur ultrices aliquet. Nam posuere pulvinar lorem, in
-              fringilla sapien volutpat at.
+              ligula porta.
             </div>
           </div>
-          <div className=' my-4 p-4 w-3/4 bg-gray-100 flex items-center justify-between'>
+          <div className=' m-4 p-4 bg-gray-100 flex items-center justify-between'>
             <img
               src='https://i.ibb.co/whGTTsq/1586501225793.jpg'
               className='h-64 mr-8'
+              alt='profile'
             />
             <div className='desc'>
+              <div className='name font-semibold'>Ashwath Niranjh</div>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
               viverra metus eget turpis rutrum, sit amet ullamcorper leo
               efficitur. Fusce efficitur sapien ac erat dignissim, quis rutrum
-              ligula porta. In semper nisi quam, in egestas leo ultrices quis.
-              In consectetur ultrices aliquet. Nam posuere pulvinar lorem, in
-              fringilla sapien volutpat at.
+              ligula porta.
+            </div>
+          </div>
+          <div className=' m-4 p-4 bg-gray-100 flex items-center justify-between'>
+            <img
+              src='https://i.ibb.co/whGTTsq/1586501225793.jpg'
+              className='h-64 mr-8'
+              alt='profile'
+            />
+            <div className='desc'>
+              <div className='name font-semibold'>Ashwath Niranjh</div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              viverra metus eget turpis rutrum, sit amet ullamcorper leo
+              efficitur. Fusce efficitur sapien ac erat dignissim, quis rutrum
+              ligula porta.
+            </div>
+          </div>
+          <div className=' m-4 p-4 bg-gray-100 flex items-center justify-between'>
+            <img
+              src='https://i.ibb.co/whGTTsq/1586501225793.jpg'
+              className='h-64 mr-8'
+              alt='profile'
+            />
+            <div className='desc'>
+              <div className='name font-semibold'>Ashwath Niranjh</div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              viverra metus eget turpis rutrum, sit amet ullamcorper leo
+              efficitur. Fusce efficitur sapien ac erat dignissim, quis rutrum
+              ligula porta.
+            </div>
+          </div>
+          <div className=' m-4 p-4 bg-gray-100 flex items-center justify-between'>
+            <img
+              src='https://i.ibb.co/whGTTsq/1586501225793.jpg'
+              className='h-64 mr-8'
+              alt='profile'
+            />
+            <div className='desc'>
+              <div className='name font-semibold'>Ashwath Niranjh</div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              viverra metus eget turpis rutrum, sit amet ullamcorper leo
+              efficitur. Fusce efficitur sapien ac erat dignissim, quis rutrum
+              ligula porta.
             </div>
           </div>
         </div>
