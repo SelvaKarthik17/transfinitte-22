1d93f144b93f9e8ecea826d7085bb23835b593bf
Arnav Menon <arnavmenonr@gmail.com>
2022-10-15 13:03:15 +0530

    Feat: Add family tree generation from JSON

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Tree from "./pages/Tree";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/tree' element={<Tree />} />
       </Routes>
     </div>
   );
