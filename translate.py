4ef8e9b69ba90b72bf978b53a2f6b80fe929ed9b
SelvaKarthik17 <57003577+SelvaKarthik17@users.noreply.github.com>
2022-10-15 14:41:47 +0530

    Translation (#3)
    * feat: tamil to english translation
    
    * refactor: improved translation speed

---
+++ translate.py
@@ -0,0 +1,60 @@
+from google.cloud import translate
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'key.json'  
+
+def translate_text(text , project_id="favorable-order-365522"):
+    # print(len(text))
+    client = translate.TranslationServiceClient()
+    location = "global"
+    parent = f"projects/{project_id}/locations/{location}"
+
+    response = client.translate_text(
+        request={
+            "parent": parent,
+            "contents": [text],
+            "mime_type": "text/plain",
+            "source_language_code": "ta",
+            "target_language_code": "en-US",
+        }
+    )
+
+    for translation in response.translations:
+
+        f = open("english-out.txt", "a")
+
+        lines = translation.translated_text.split("\n")
+        non_empty_lines = [line for line in lines if line.strip() != ""]
+
+        string_without_empty_lines = ""
+
+        for line in non_empty_lines:
+            string_without_empty_lines += line + "\n"
+
+        f.write(string_without_empty_lines)
+        f.close()
+
+
+tamil_text = ""
+
+with open('text-in.txt', 'r') as file:
+
+    line = file.readline()
+    cnt = 1
+    while line:
+        line = file.readline()
+        tamil_text = tamil_text + line + "\n"
+        cnt += 1
+
+
+l = len(tamil_text)
+
+while l > 0:
+    if l > 30000:
+        translate_text(tamil_text[:30000])
+        tamil_text = tamil_text[30000:]
+        l = len(tamil_text)
+    else:
+        translate_text(tamil_text)
+        l = 0
+
