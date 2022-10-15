d5a2377eac6d45ba6ddee5d5efa4532dcfdaf602
ashwath niranjh <ashwathniranjh@gmail.com>
2022-10-15 09:01:16 +0530

    Feat: Created page directory, added about page with placeholder data

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import Body from "../components/Body";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
